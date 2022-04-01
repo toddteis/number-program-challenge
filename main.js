a2318e9d7c3ea07622ef1815e61f73f063f9ad2b
toddt <toddteis@gmail.com>
2022-04-01 14:11:01 +1000

    Added sectional DOM variables & timer().

--- main.js
+++ main.js
@@ -1,13 +1,36 @@
+
+// User Interface
+
 const btnTimerSubmit = document.getElementById("btn-timer-submit");
-// let setTimerInterval;
+const timerInputError = document.getElementById("timer-input-error");
+const setIntervalDiv = document.getElementById("set-interval");
+const displayTimerDiv = document.getElementById('display-timer-wrapper');
+const numberInDiv = document.getElementById('number-in');
+const displayDiv = document.getElementById('display');
 
 btnTimerSubmit.addEventListener('click', () => {
     const timerTextInput = document.getElementById("timer-text-input").value;
     const setTimerInterval = Number(timerTextInput);
 
+    // Check if not a number or 0 was set and show error.
     if (isNaN(setTimerInterval) || setTimerInterval == 0) {
-        console.log('Try again')
+        timerInputError.classList.remove('hide');
     } else {
-        console.log('is a number');
+        // if incorrect interval input previous, hide error message
+        if (!timerInputError.classList.contains('hide')) { timerInputError.classList.toggle('hide') };
+        // start interval timer
+        timer(setTimerInterval);
+        // hide set-interval div
+        // show display-timer-wrapper
     }
 });
+
+
+
+// Core logic
+
+function timer(para) {
+    const intervalInMS = para * 1000;
+    console.log(`start interval timer is this interval: ${intervalInMS}ms`);
+};
+
